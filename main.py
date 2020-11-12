61adeea14c14e6af92cdd862392f36f4656f1e2b
Eugen Zdrincu <eugenzdrincu@gmail.com>
2020-11-12 17:03:10 +0200

    extract files to temp folder

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+import glob
 import argparse
+
+from zipfile import ZipFile, ZipInfo
 
 #Parse Arguments
 parser = argparse.ArgumentParser(description="Automatizare pentru export fotografii Canva")
@@ -14,6 +18,30 @@ DESTINATION_FOLDER_NAME = "proccessed"
 DESTINATION_FOLDER_PATH = SOURCE_FOLDER_PATH + '/' + DESTINATION_FOLDER_NAME
 
 def proccess(dir_path):
-    print(dir_path)
+    #Creates appdata dir if it doesen't exist
+    tempdir = os.path.join(SOURCE_FOLDER_PATH, 'temp')
+    print(tempdir + '\n')
+    if not os.path.exists(tempdir):
+        os.mkdir(tempdir)
+
+    #Change dir to SOURCE_PATH_FOLDER
+    os.chdir(dir_path)
+    #Create dest dir if it doesen't exist
+    if not os.path.exists(DESTINATION_FOLDER_PATH):
+        os.makedirs(DESTINATION_FOLDER_NAME)
+
+    #Foreach zip file unpack it in the dest dir
+    for file in glob.glob("*.zip"):
+        print(f"Found file {file}")
+        print("-------------------")
+
+        with ZipFile(file, 'r') as zip:
+            zip.extractall(os.path.join(tempdir, file))
+
+        for photo in os.listdir(os.path.join(tempdir, file)):
+            print(photo)
+
+        print("\n")
+
 
 proccess(SOURCE_FOLDER_PATH)
