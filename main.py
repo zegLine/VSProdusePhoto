9a867cc591ba8190414227843978dd89fce54e24
Eugen Zdrincu <eugenzdrincu@gmail.com>
2020-11-12 17:15:38 +0200

    finalized working prototype

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
 import glob
 import argparse
+import shutil
 
-from zipfile import ZipFile, ZipInfo
+from zipfile import ZipFile
 
 #Parse Arguments
 parser = argparse.ArgumentParser(description="Automatizare pentru export fotografii Canva")
 parser.add_argument('--source', default="C:/vsphotos", help="Path-ul catre directorul cu fisierele .zip / .rar (necesar)")
-parser.add_argument('--dest', default="", help="Numele folderului destinatie (optional)")
+parser.add_argument('--dest', default="proccessed", help="Numele folderului destinatie (optional)")
 args = parser.parse_args()
 
 #Source path
 SOURCE_FOLDER_PATH = args.source
 
 #Destination name and path (default at ./proccessed)
-DESTINATION_FOLDER_NAME = "proccessed"
+DESTINATION_FOLDER_NAME = args.dest
 DESTINATION_FOLDER_PATH = SOURCE_FOLDER_PATH + '/' + DESTINATION_FOLDER_NAME
 
 def proccess(dir_path):
@@ -35,11 +36,24 @@ def proccess(dir_path):
         print(f"Found file {file}")
         print("-------------------")
 
-        with ZipFile(file, 'r') as zip:
-            zip.extractall(os.path.join(tempdir, file))
+        filetempdir = os.path.join(tempdir, file)
 
-        for photo in os.listdir(os.path.join(tempdir, file)):
-            print(photo)
+        with ZipFile(file, 'r') as zip:
+            zip.extractall(filetempdir)
+
+        for photo in os.listdir(filetempdir):
+            photopath = os.path.join(filetempdir, photo)
+
+            filefinalname = f'{file[:-4]} ({photo[:-4]}).{photo[-3:]}'
+            filefinalpath = os.path.join(DESTINATION_FOLDER_PATH, filefinalname)
+
+
+            try:
+                shutil.copy(photopath, filefinalpath)
+                print(f"Renamed and moved {photopath} to {filefinalpath}")
+            except:
+                print(f"There has been an error while trying to copy file {photo}")
+
 
         print("\n")
 
